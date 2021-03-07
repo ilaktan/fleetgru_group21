1dbf26b43a8569e20c8bde42f4d073071bd65353
darynatkm <65510511+darynatkm@users.noreply.github.com>
2021-03-07 17:29:39 +0100

    renamed AddEventPage.java and VehiclesPage.java and deleted CarInfoPageDT.java

--- src/test/java/com/fleetgru/pages/VehiclesPage.java
+++ src/test/java/com/fleetgru/pages/VehiclesPage.java
@@ -3,7 +3,7 @@ package com.fleetgru.pages;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
-public class VehiclesPageDT extends BasePage{
+public class VehiclesPage extends BasePage{
 
     @FindBy(xpath="//a[@title='Create Car']")
     public WebElement createCarButton;
