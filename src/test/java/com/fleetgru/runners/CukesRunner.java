5f45990bc1d1c193cf87122ba05bf8b284581f59
Cansu35 <71334287+Cansu35@users.noreply.github.com>
2021-03-09 00:11:27 +0100

    FLEET546 US's all test cases passed and  executionresults imported to the jira

--- src/test/java/com/fleetgru/runners/CukesRunner.java
+++ src/test/java/com/fleetgru/runners/CukesRunner.java
@@ -13,7 +13,7 @@ import org.junit.runner.RunWith;
         features = "src/test/resources/features",
         glue = "com/fleetgru/stepdefinitions",
         dryRun = false,
-        tags = "@FLEET-545"
+        tags = "@FLEET-584"
 
 )
 public class CukesRunner {
