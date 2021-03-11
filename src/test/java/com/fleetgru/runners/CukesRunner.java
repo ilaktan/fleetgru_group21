a8acff6ed082084bf5d775030d90558a13cb1e47
Cansu35 <71334287+Cansu35@users.noreply.github.com>
2021-03-11 21:18:09 +0100

    Smoke Test Case created and added to the Smoke Suit feature

--- src/test/java/com/fleetgru/runners/CukesRunner.java
+++ src/test/java/com/fleetgru/runners/CukesRunner.java
@@ -13,7 +13,7 @@ import org.junit.runner.RunWith;
         features = "src/test/resources/features",
         glue = "com/fleetgru/stepdefinitions",
         dryRun = false,
-        tags = "@wip"
+        tags = "@FLEET-658"
 )
 public class CukesRunner {
 }
