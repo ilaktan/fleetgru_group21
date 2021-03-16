354fddd76cb8166b86b307a98c51274ff33c5e72
Emine Çevik <emineceviik34@gmail.com>
2021-03-16 08:46:11 +0100

    Updated VehiclesPage and FLEET538StoreManagerFilterFunctions.feature, FLEET538SalesManagerFilterFunctions.feature, FLEET538_546FilterFunctionsStepDef

--- src/test/java/com/fleetgru/stepdefinitions/FLEET538_546FilterFunctionsStepDef.java
+++ src/test/java/com/fleetgru/stepdefinitions/FLEET538_546FilterFunctionsStepDef.java
@@ -77,7 +77,7 @@ public class FLEET538_546FilterFunctionsStepDef {
 
     @When("the user go to page {string}")
     public void the_user_go_to_page(String pageNum) {
-        BrowserUtils.waitFor(3);
+        BrowserUtils.waitFor(5);
         JavascriptExecutor jse = (JavascriptExecutor) Driver.get();
         jse.executeScript("arguments[0].value=';'", vehicle.pageInputBox);
         vehicle.pageInputBox.sendKeys(pageNum);
@@ -88,14 +88,14 @@ public class FLEET538_546FilterFunctionsStepDef {
     @When("the user click the refresh button")
     public void the_user_click_the_refresh_button() {
 
-        BrowserUtils.waitFor(3);
+        BrowserUtils.waitFor(5);
         vehicle.refreshFilterButton.click();
     }
 
 
     @When("the user click the reset button")
     public void the_user_click_the_reset_button() {
-        BrowserUtils.waitFor(2);
+        BrowserUtils.waitFor(5);
         vehicle.closeGridSetting.click();
         JavascriptExecutor javascriptExecutor = (JavascriptExecutor) Driver.get();
         javascriptExecutor.executeScript("arguments[0].click();", vehicle.resetButton2);
@@ -115,14 +115,13 @@ public class FLEET538_546FilterFunctionsStepDef {
 
     @When("user click the {string} column")
     public void user_click_the_column(String columnName) {
-        BrowserUtils.waitFor(3);
+        BrowserUtils.waitFor(5);
         vehicle.clickColumn(columnName);
-        BrowserUtils.waitFor(1);
     }
 
     @Then("the system should display {string} column on the webpage")
     public void the_system_should_display_column_on_the_webpage(String columnName) {
-
+        BrowserUtils.waitFor(5);
         List<String> actualColumnList = BrowserUtils.getElementsText(vehicle.tableColumnSize);
 
         System.out.println("actualColumnList = " + actualColumnList);
@@ -140,6 +139,7 @@ public class FLEET538_546FilterFunctionsStepDef {
 
     @Then("the system should not display {string} column on the webpage")
     public void the_system_should_not_display_column_on_the_webpage(String columnName) {
+        BrowserUtils.waitFor(5);
 
         List<String> actualColumnList = BrowserUtils.getElementsText(vehicle.tableColumnSize);
 
@@ -173,21 +173,21 @@ public class FLEET538_546FilterFunctionsStepDef {
 
     @When("the user click the filter button")
     public void the_user_click_the_filter_button() {
+        BrowserUtils.waitFor(5);
         vehicle.filtersButton.click();
-        BrowserUtils.waitFor(1);
     }
 
 
     @When("the user click the manage filter button")
     public void the_user_click_the_manage_filter_button() {
-        BrowserUtils.waitFor(1);
+        BrowserUtils.waitFor(4);
         vehicle.manageFilters.click();
 
     }
 
     @When("User searches {string} column in the Search input")
     public void user_searches_column_in_the_Search_input(String columnName) {
-
+        BrowserUtils.waitFor(5);
         vehicle.manageFilterSearchInput.sendKeys(columnName);
         BrowserUtils.waitFor(1);
 
@@ -195,7 +195,7 @@ public class FLEET538_546FilterFunctionsStepDef {
 
     @Then("verifies driver not displayed")
     public void verifies_driver_not_displayed() {
-
+        BrowserUtils.waitFor(5);
         String expected = "";
         String actual = vehicle.tableManageFilter.getText();
         Assert.assertEquals("verify match the column name", actual, expected);
@@ -206,7 +206,7 @@ public class FLEET538_546FilterFunctionsStepDef {
 
     @Then("following columns should be available under manage filter button")
     public void following_columns_should_be_available_under_manage_filter_button(List<String> expectedFilterOptions) {
-        BrowserUtils.waitFor(2);
+        BrowserUtils.waitFor(5);
 
         List<String> actualFilterOptions = BrowserUtils.getElementsText(vehicle.listFilterOptions);
 
@@ -220,7 +220,7 @@ public class FLEET538_546FilterFunctionsStepDef {
 
     @Then("the system should display the {string} option under dropdown menu")
     public void the_system_should_display_the_option_under_dropdown_menu(String expectedColumnName) {
-
+        BrowserUtils.waitFor(5);
         String actual = vehicle.tableManageFilter.getText();
         Assert.assertTrue("verify match the column name", actual.contains(expectedColumnName));
         System.out.println("expectedColumnName = " + expectedColumnName);
@@ -231,15 +231,14 @@ public class FLEET538_546FilterFunctionsStepDef {
 
     @When("the user click the {string} button")
     public void the_user_click_the_button(String filterName) {
-        BrowserUtils.waitFor(1);
+        BrowserUtils.waitFor(5);
         vehicle.selectManageFilterOptions(filterName);
     }
 
     @Then("the user should be able to see the {string} on the filter container")
     public void the_user_should_be_able_to_see_the_on_the_filter_container(String expectedFilter) {
 
-        BrowserUtils.waitFor(1);
-
+        BrowserUtils.waitFor(5);
         String actual = vehicle.filterSelectorBox.getText();
         Assert.assertTrue("verify filter box", actual.contains(expectedFilter));
         System.out.println("expectedFilter = " + expectedFilter);
@@ -250,24 +249,26 @@ public class FLEET538_546FilterFunctionsStepDef {
 
     @When("the user click the {string} button on the filter container")
     public void the_user_click_the_button_on_the_filter_container(String filterName) {
-
+        BrowserUtils.waitFor(5);
         vehicle.manageSelectedFilter(filterName);
 
     }
 
     @Then("verifies Contains dropdown menu displayed")
     public void verifies_Contains_dropdown_menu_displayed() {
+        BrowserUtils.waitFor(5);
         Assert.assertEquals("Contains", vehicle.containsDefault.getText());
     }
 
     @Given("user clicks on Contains button;")
     public void user_clicks_on_Contains_button() {
+        BrowserUtils.waitFor(5);
         vehicle.containsDefault.click();
     }
 
     @Then("user should be able to see following dropdown menu options")
     public void user_should_be_able_to_see_following_dropdown_menu_options(List<String> expectedDropdownOptions) {
-        BrowserUtils.waitFor(2);
+        BrowserUtils.waitFor(5);
         List<String> actualDropdownOptions = BrowserUtils.getElementsText(vehicle.containsDropdownMenu);
         System.out.println("expectedDropdownOptions = " + expectedDropdownOptions);
         System.out.println("actualDropdownOptions = " + actualDropdownOptions);
@@ -279,16 +280,16 @@ public class FLEET538_546FilterFunctionsStepDef {
 
     @When("the search {string} in the search box")
     public void the_search_in_the_search_box(String string) {
-        BrowserUtils.waitFor(3);
+        BrowserUtils.waitFor(5);
         vehicle.filterSelectorSearchInput.sendKeys(string);
-        BrowserUtils.waitFor(3);
+        BrowserUtils.waitFor(5);
         vehicle.updateButton.click();
     }
 
     @Then("The system should only show records that contain the Location {string}")
     public void the_system_should_only_show_records_that_contain_the_Location(String expected) {
 
-        BrowserUtils.waitFor(2);
+        BrowserUtils.waitFor(5);
 
         List<String> actual = BrowserUtils.getElementsText(vehicle.locationColumn);
 
